0eddb4feab84b2e6a92ead195d1f276aa68bac20
pawan327 <57008201+pawan327@users.noreply.github.com>
2021-02-04 14:50:04 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,6 +1,6 @@
 def bubble_sort(list):
 
-//Swaping the elements to arrange in order
+##Swaping the elements to arrange in order
     for iter_num in range(len(list)-1,0,-1):
         for idx in range(iter_num):
             if list[idx] > list[idx+1]:
